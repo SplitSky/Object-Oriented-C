051b23b7571b062254b01a8209eaa51a21a24fd4
SplitSky <opop.neska60@gmail.com>
2021-02-14 15:12:23 +0000

    assignment 1

--- GeneralCode/GeneralCode.cpp
+++ GeneralCode/GeneralCode.cpp
@@ -1,12 +1,6 @@
 // GeneralCode.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
@@ -18,3 +12,47 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+// Assignment 1 - skeleton code
+// Your name and date
+// Program to calculate transition energy using simple Bohr formula
+
+#include<iostream>
+#include<iomanip>
+#include<string>
+
+int main()
+{
+    // Declare variables here
+    constant double electron_volt{ 1.6E-19 };
+    int Z{ 0 };
+    int n_1{ 0 };
+    int n_2{ 0 };
+    double E{ 0 };
+    // Ask user to enter atomic number
+    std::cout << "Enter the atomic number: " << std::endl;
+    std::cin >> Z;
+    while (std::cin.fail()) {
+        std::cout << "The atomic number is incorrect";
+        std::cin.clear();
+        std::cin.ignore();
+        std::cin >> Z;
+    }
+    // Ask user to enter initial and final quantum numbers
+    std::cout << "Enter the first quantum number n: " << std::endl;
+    std::cin >> n_1;
+    // validation
+
+    std:cout << "Enter the second quantum number n: " << std::endl;
+    std::cin >> n_2;
+
+
+    // Compute photon energy, Delta E = 13.6*(Z^2)*(1/n_j^2-1/n_i^2) eV
+    std:cout << "Should it be displayed in eV or Joules? (e/J)" << std::endl;
+
+
+
+    // Output answer
+
+    return 0;
+}
