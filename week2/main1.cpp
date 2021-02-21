385d03b3a7275b3f071be848a6c859ee81c20386
Splitsky <opop.neska60@gmail.com>
2021-02-21 00:35:56 +0000

    initial edits

--- week2/main1.cpp
+++ week2/main1.cpp
@@ -1,6 +1,7 @@
 
 // PHYS 30762 Programming in C++
 // Assignment 2
+// Tomasz Neska 20/02/2021
 
 // Program to compute mean, standard deviation and standard
 // error of the a set of courses. Data is read from file
@@ -11,42 +12,36 @@
 #include<iomanip>
 #include<fstream>
 #include<cmath>
+#include<string>
 
 // Functions to compute mean and standard deviation
 
 // Main function
 
 int main() {
-  char data_file[100];
-
-  // Ask user to enter number of courses
-  // Or determine in another way
-  int number_courses{};
-
-  // Ask user to enter filename
-  std::cout<<"Enter data filename: ";
-  std::cin>>data_file;
-
-  // Open file and check if successful
-  std::fstream course_stream(data_file);
-
-  // Allocate memory for data 
-
-  // Read data from file
-  for(int i{};i<Ndata;i++) {
-
+    // import the file
+    std::fstream my_file("courselist.dat");
+    if (!my_file.good()) {
+        // print out error message
+        std::cout << "The file couldn't be loaded";
+        return -1;
     }
 
-  // Close file
-  course_stream.close();
-
-  // Print number of courses read in
-
-  // Compute mean, standard deviation and  standard error of mean
-
-  // Free memory
-
-
-  return 0;
+    bool test2{};
+    char data_array[100];
+    my_file>>data_array;
+    my_file>>test2;
+    std::cout << data_array << std::endl;
+    std::cout << test2;
+    
+    // Ask user to enter number of courses
+    // Or determine in another way
+    //int number_courses{};
+    // Ask user to enter filename
+    //std::cout<<"Enter data filename: ";
+    //std::cin>>data_file;
+    // Open file and check if successful
+    //std::fstream course_stream(data_file);
+    return 0;
 }
 
