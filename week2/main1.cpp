4ab71d13aac11c368050b5a8033a2e1dad81a4cd
SplitSky <opop.neska60@gmail.com>
2021-02-21 13:58:05 +0000

    sorting out reading in files

--- week2/main1.cpp
+++ week2/main1.cpp
@@ -19,21 +19,40 @@
 // Main function
 
 int main() {
-    // import the file
-    std::fstream my_file("courselist.dat");
-    if (!my_file.good()) {
-        // print out error message
+	int N{0};
+	std::string file_line;
+	std::ifstream my_file("courselist.dat");
+	if (!my_file.good()) {
         std::cout << "The file couldn't be loaded";
         return -1;
-    }
+    } else {
+		// count the number of lines in the file
+		while(!my_file.eof()) {
+			getline(my_file,file_line);
+			std::cout << "File Line: " << file_line << std::endl;
+			N++;
+		}
+		my_file.close();
+			
+	}
 
-    bool test2{};
-    char data_array[100];
-    my_file>>data_array;
-    my_file>>test2;
-    std::cout << data_array << std::endl;
-    std::cout << test2;
-    
+	std::ifstream my_file("courselist.dat");
+	
+	//float *data_array{new float[N]};
+	std::string *data_array{new std::string[N]};
+	std::cout << data_array;
+	
+	for(int i=0;i<N;i++) {
+		getline(my_file,file_line);
+		data_array[i] = file_line;
+		
+		std::cout << "File Line: " << file_line << std::endl;
+		std::cout << "Data content: " << data_array << std::endl;
+		std::cout << i << std::endl;
+		
+		
+		
+	}
     // Ask user to enter number of courses
     // Or determine in another way
     //int number_courses{};
@@ -42,6 +61,14 @@ int main() {
     //std::cin>>data_file;
     // Open file and check if successful
     //std::fstream course_stream(data_file);
+	delete[] data_array;
     return 0;
+	my_file.close();
+	
+	
+	////
+	
+	// open file first time
+	//
 }
 
