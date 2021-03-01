158e4fa917ec2a1d933e3d2c17c770a3bb49130a
Splitsky <opop.neska60@gmail.com>
2021-03-01 15:55:01 +0000

    half way through week 2

--- week2/main2.cpp
+++ week2/main2.cpp
@@ -11,44 +11,91 @@
 #include<iomanip>
 #include<fstream>
 #include<cmath>
-
+#include<tuple>
+#include<vector>
+#include<string>
+#include<algorithm>
 // Functions to compute mean and standard deviation
+double calculate_mean(std::string array) {
+    double mean;
+    double standard_deviation;
+    double std_error;
 
-// Main function
+    // the calculation depends on the format of the array
+    double return_array[3];
+    return_array[0] = mean;
+    return_array[1] = standard_deviation;
+    return_array[2] = std_error;
+    
+    return mean; 
+} 
 
-int main()
-{
-  char data_file[100];
+int count_courses(std::string file_name){
+    int N{0};
+    std::string file_line;
+    std::ifstream my_file(file_name);
 
-  // Ask user to enter number of courses
-  // Or determine in annother way
-  int number_courses;
-
-  // Ask user to enter filename
-  std::cout<<"Enter data filename: ";
-  std::cin>>data_file;
-
-  // Open file and check if successful
-  std::fstream course_stream(data_file);
-
-  // Allocate memory for data 
-
-  // Read data from file
-  for(int i{};i<Ndata;i++) 
-    {
-
+    // count the number of lines in the file
+    while(!my_file.eof()){
+        getline(my_file,file_line);
+        N++;
     }
 
-  // Close file
-  course_stream.close();
+    my_file.close();
+    return N;
+}
 
-  // Print number of courses read in
+// Main function
+int main() {
+    std::string data_file_name;
+    bool validation = false;
+    int number_courses{0};
+    std::string file_line;
+    // Ask user to enter filename
+    std::cout<<"Enter data filename: ";
+    std::cin>>data_file_name;
+    // Open file and check if successful
+    std::ifstream course_stream(data_file_name);
+    
+    while (!validation) {
+        std::cout << "The file couldn't be loaded. Try again.";
+        std::cin>>data_file_name;
+        std::ifstream course_stream(data_file_name); 
+        validation = course_stream.good();
+    }
 
-  // Compute mean, standard deviation and  standard error of mean
+    //number_courses = count_courses(data_file_name); // may not be needed
 
-  // Free memory
+    // Allocate memory for data 
+    //std::string *full_data{new std::vector<std::string>(number_courses)};
+    std::vector<std::string> full_data; // suppose it works like a python list
+    // declares the data array
 
+    // Read data from file
+    for(int i{0};i<number_courses;i++) {
+        getline(course_stream,file_line);
+        full_data.push_back(file_line);
+        number_courses ++;
+    }
 
-  return 0;
+    // Close file
+    course_stream.close();
+    
+    // testing start
+    std::cout << "The file array printing" << std::endl;
+    std::cout << full_data[0] << std::endl;
+    // testing end
+
+    // Print number of courses read in
+    std::vector<std::string>::iterator vector_begin{full_data.begin()};
+    std::vector<std::string>::iterator vector_end{full_data.end()};
+    std::vector<std::string>::iterator vector_iterator; // check whether you can run it without matching the type
+    for (vector_iterator = vector_begin; vector_iterator<vector_end; ++vector_iterator) std::cout << *vector_iterator << std::endl;
+    // Compute mean, standard deviation and  standard error of mean
+
+    // Free memory
+    
+
+    return 0;
 }
 
--- week2/test.cpp
+++ week2/test.cpp
@@ -1,12 +1,29 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<cmath>
+#include<tuple>
+#include<vector>
+#include<string>
+#include<algorithm>
+
 
 int main() {
-    std::string str = "123.4123";
-    float num_float = std::stof(str);
-    double num_double = std::stod(str);
+    std::vector<std::string> double_vector;
 
-    std::cout << num_double << std::endl;
-    std::cout << num_float << std::endl;
+    double_vector.push_back("word1");
+    double_vector.push_back("word2");
+    double_vector.push_back("word3");
+
+    std::vector<std::string>::iterator vector_begin{double_vector.begin()};
+    std::vector<std::string>::iterator vector_end{double_vector.end()};
+
+    std::sort(vector_begin, vector_end);
+
+    std::cout<<"sorted data"<<std::endl;
+    std::vector<std::string>::iterator vector_iterator;
+    for(vector_iterator=vector_begin; vector_iterator<vector_end;++vector_iterator)
+    std::cout <<*vector_iterator<<std::endl;
+    return 0;    
 
 }
