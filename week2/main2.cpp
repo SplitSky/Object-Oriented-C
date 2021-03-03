b89233da03a1209ee19aa2de3f9c065937abc5b9
Splitsky <opop.neska60@gmail.com>
2021-03-03 18:24:40 +0000

    main 2 works. It's bubble. Fix to quick or merge

--- week2/main2.cpp
+++ week2/main2.cpp
@@ -25,56 +25,87 @@ struct list_node {
 };
 
 // other functions
-bool compare_two_nodes_title(list_node a, list_node b) {
-    return a.title > b.title;
-}
-
-bool compare_two_nodes_code(list_node a, list_node b) {
-    return a.unit_code > b.unit_code;
-}
-
-void compute_ranks(list_node a[], int n, int data_size,bool type) {
-    if (type) {
-        std::sort(a, a + data_size, compare_two_nodes_title);    
-    } else {
-        std::sort(a, a + data_size, compare_two_nodes_code);
-    }
-
-    for (int i{0}; i<n; i++) {
-        a[i].rank = i + 1;
-    }
-    return; // be weary of using the variable or pointer array
-}
 
 
-
-double calc_mean(std::vector<double> data_array,int N) {
+double calc_mean(std::vector<list_node> data_array) {
+    int N = data_array.size();
     double sum{0};
     for (int i{0};i<=N;i++) {
-        sum += data_array[i];
+        sum += data_array[i].mark;
     }
     return sum/static_cast<double>(N);
 }
 
-double calc_std(std::vector<double> data_array, int N, double mean) {
+double calc_std(std::vector<list_node> data_array, double mean) {
+   
+    int N = data_array.size();
     double sum{0};
     for (int i{0}; i<=N; i++) {
-        sum += pow(data_array[i]-mean,2);
+        sum += pow(data_array[i].mark-mean,2);
     }
     return pow(sum/static_cast<double>(N), 0.5);
 }
+// c++ does things when you use & cuz yes
+void bubble_sort(std::vector<list_node>& array, int type) {
+    int size = array.size();
+    bool swap{true};
+    list_node holder;
+ 
+    if (type==1) {
+        while(swap==true) {
+            swap = false;
+            for(int i{1};i<size;i++) {
+                if (array[i-1].title > array[i].title) {
+                    holder.mark = array[i-1].mark;
+                    holder.unit_code = array[i-1].unit_code;
+                    holder.title = array[i-1].title;
 
+                    array[i-1].mark = array[i].mark;
+                    array[i-1].unit_code = array[i].unit_code;
+                    array[i-1].title = array[i].title;
+
+                    array[i].mark = holder.mark;
+                    array[i].unit_code = holder.unit_code;
+                    array[i].title = holder.title;
+                    swap = true;
+                }
+            }
+        }      
+    } else {
+         while(swap==true) {
+            swap = false;
+            for(int i{1};i<size;i++) {
+                if (array[i-1].unit_code > array[i].unit_code) {
+
+                    holder.mark = array[i-1].mark;
+                    holder.unit_code = array[i-1].unit_code;
+                    holder.title = array[i-1].title;
+
+                    array[i-1].mark = array[i].mark;
+                    array[i-1].unit_code = array[i].unit_code;
+                    array[i-1].title = array[i].title;
+
+                    array[i].mark = holder.mark;
+                    array[i].unit_code = holder.unit_code;
+                    array[i].title = holder.title;
+                    swap = true;
+                }
+            }
+        }       
+    }
+    return;
+}
 // Main function
 int main() {
-    double calc_mean(std::vector<double> data_array, int N);
-    double calc_std(std::vector<double> data_array, int N, double mean);
+    list_node temporary;  
+    double calc_mean(std::vector<list_node> data_array);
+    double calc_std(std::vector<list_node> data_array, double mean);
+    
 
     std::string data_file_name;
     int number_courses{0};
     std::string file_line;
     // Ask user to enter filename
-    //std::cout<<"Enter data filename: ";
-    //std::cin>>data_file_name;
     data_file_name = "courselist.dat";
     // Open file and check if successful
     std::ifstream course_stream(data_file_name);
@@ -83,16 +114,24 @@ int main() {
        return -1;
     }
     // Allocate memory for data 
-    //std::string *full_data{new std::vector<std::string>(number_courses)};
     std::vector<std::string> full_data; // the unit titles
-    std::vector<double> scores; //just the numerical scores
     // declares the data array
-
+    std::vector<list_node> chosen_data;
+    std::vector<list_node> sliced_data;
+    
     while(!course_stream.eof()) {
         getline(course_stream,file_line); // counts only non empty lines
         if(file_line.length() != 0) {
+            temporary.mark = std::stod(file_line.substr(0,4));
+            temporary.unit_code = std::stoi(file_line.substr(5,9));
+            temporary.title = file_line.substr(13,file_line.length());
+
+
+            sliced_data.push_back(temporary);
+
             full_data.push_back(file_line.substr(5,file_line.length()));
-            scores.push_back(std::stod(file_line.substr(0,4)));
+            //scores.push_back(std::stod(file_line.substr(0,4)));
+            
             number_courses ++;
         }
     }
@@ -107,27 +146,9 @@ int main() {
         std::cout << "PHYS "<< *vector_iterator << std::endl;
     }
 
-    std::cout << "The data from the second array: " << std::endl;
-    std::vector<double>::iterator vector_begin2{scores.begin()};
-    std::vector<double>::iterator vector_end2{scores.end()};
-    std::vector<double>::iterator vector_iterator2;
-
-    for (vector_iterator2=vector_begin2; vector_iterator2<vector_end2; ++vector_iterator2){
-        std::cout << *vector_iterator2 << std::endl;
-    }
-
-    //double sum{0};
-    //for(int i{0};i<=number_courses;i++) {
-    //    sum += scores[i];
-    //}
-    double mean = calc_mean(scores, number_courses);
-    //double mean = 1/static_cast<double>(number_courses) * sum;
-    //sum = 0;
-    //for(int i{0};i<=number_courses;i++) {
-    //    sum += pow(scores[i] - mean, 2);
-    //}
-    //double standard_deviation{pow(1/static_cast<double>(number_courses) * sum,0.5)};
-    double standard_deviation{calc_std(scores,number_courses,mean)};
+    
+    double mean = calc_mean(sliced_data);
+    double standard_deviation{calc_std(sliced_data,mean)};
     double std_error = standard_deviation/(pow(static_cast<double>(number_courses),0.5));
 
     // Compute mean, standard deviation and  standard error of mean
@@ -135,6 +156,7 @@ int main() {
     std::cout << std::setprecision(3) << "The standard deviation is: " << standard_deviation << " +- " << std_error <<std::endl;
     std::cout << std::endl;
     
+
     int year{0};
     // print out custom range
     std::cout << "Enter the year you want to display: " << std::endl;
@@ -145,35 +167,49 @@ int main() {
         std::cin.ignore();
         std::cin >> year;
     }
+    
 
-    while (year != '1' || year !='2' || year !='3' || year !='4') {
-        std::cout << "Error 2: The number if incorrect";
-        std::cin.clear();
-        std::cin.ignore();
-        std::cin >> year;
-    }
+    // Choose the data
+    int choice;
+    std::cout << "Sort by [1]title or by [2]unit_code: ";
+    std::cin >> choice;
 
-    // assign a new array
-    std::vector<std::string> sliced_scores;
     std::string temp;
-    for(int i{};i<=number_courses;i++) {
-        // test = std::to_string(score[i])[0]
-        //if(full_data[i][0]==year) {
-            // scrape the data you want
-        //    temp = std::to_string(scores[i]) + " " + full_data[i];
-        //    sliced_scores.push_back(temp); /// make sure you develop a nested system {[score, code, title] ,[score, code, title] ,[etc] } then
-            // sort the rest of it using the algorithm from above
-        std::cout << full_data[i][0] << std::endl;
-        //}
+    std::cout << "The data for the selected range";
+
+    std::cout << sliced_data.size() << std::endl;
+
+
+    for(int i{0}; i<sliced_data.size();i++) {
+        if(sliced_data[i].unit_code/ 10000 == year) { 
+            chosen_data.push_back(sliced_data[i]);
+            std::cout << "PHYS" << std::to_string(sliced_data[i].unit_code) << " " << sliced_data[i].title <<std::endl;
+            
+        } 
     }
+    
+    
+
+    // calculate the mean
+    mean = calc_mean(chosen_data);
+    // calculate the standard deviation
+    standard_deviation = calc_std(chosen_data,mean);
+    // calculate the standard deviation error
+    std_error = standard_deviation/(pow(static_cast<double>(number_courses),0.5));
+
+    
+    // sorting the data
+    bubble_sort(chosen_data,choice); // pass in a reference. Don't return anything just alter memory
+
+    //print out data
+    for(int i{};i<chosen_data.size(); i++) {
+        std::cout << "PHYS" << chosen_data[i].unit_code << " " << chosen_data[i].title << std::endl;
+    }
+    
+    std::cout << std::setprecision(3) << "The mean for the full data set is: " << mean << std::endl;
+    std::cout << std::setprecision(3) << "The standard deviation is: " << standard_deviation << " +- " << std_error <<std::endl;
+    std::cout << std::endl;
 
     return 0;
-
-    // things to do
-    // use the iterator with the printing of all information
-    // print out mean and all other calculations for the selection
-    // sort the list by the title or course code
-    // keep 3 differnt components as differnt arrays
-    // when you are about to print out concatenate the strings together into a single array and use an iterator.
 }
 
