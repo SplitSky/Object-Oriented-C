634f97675a62e825d6b289dbb92a23ed3f41ef4b
Splitsky <opop.neska60@gmail.com>
2021-04-23 01:38:15 +0100

    first commit assign 6

---
+++ week6/main.cpp
@@ -0,0 +1,32 @@
+// Object oriented C++ - Assignment 6 - Classes
+// Tomasz Neska
+// 22/04/2021
+#include<fstream>
+#include<iostream>
+#include<cmath>
+#include<vector>
+#include<string>
+#include<array>
+#include<iterator>
+#include<cstring>
+
+// things to do
+// 1. Abstract class used as an interface for all shapes
+// 2. Abstract derived class for a) 2D shapes b) 3D shapes
+// 3. Derived classes for at least 4 2D shapes (rectangle, square ellipse, circle)
+// 4. Derived classes for at least 4 3D shapes (cuboid, cube, ellipsoid, sphere)
+// 5. Additional ingeritance for specialisation ("is a")
+// 6. Constructors and virtual destructors
+// 7. Pure virtual fuctions to return area and volume (can be zero for 2D shapes)
+// Main must:
+// Implement an vector of at least 8 base class pointers, each pointing to a different shape
+// Output the area and volume for 3D shapes
+// Clean up by deleting objects and arrays when finished
+
+// abstract class
+
+
+int main() {
+
+    return 0;
+}
