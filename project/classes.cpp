0b2e759a1ca786ad40db86bfb24fdf1ddebbfd94
Splitsky <opop.neska60@gmail.com>
2021-05-07 23:55:56 +0100

    small updates and structure

--- project/classes.cpp
+++ project/classes.cpp
@@ -1,24 +1,29 @@
 // this file contains classes
 #include<string>
+#include<vector>
+#include<map>
 //
 // copy and move constructors only used for the board class
 //
 class piece {
     protected:
         // protected variables
-        std::string position; // two character position e.g. A1
+        int position[2]; // array of two numbers. First is letter second is number i.e. [1,1] is A1
         std::string name; // e.g. pawn
         int point_value; // the colour depends on the sign of the point value. black is positive
         
     public:
         piece() {
-            position = "00";    
+            position[0] = 1;
+            position[1] = 1;
+            name = "empty";
+            point_value = 0;
         };
-        piece(std::string init_position, std::string init_name, bool init_colour) : position{init_position}, name{init_name} {};
+        piece(int init_position, std::string init_name, bool init_colour) : position{init_position}, name{init_name} {};
         virtual ~piece() {};
 
         std::string get_pos() {
-            return this->position;
+            return std::to_string(this->position[0]) + "," + std::to_string(this->position[1]);
         }
 };
 
@@ -26,16 +31,36 @@ class pawn : public piece {
     protected:
         // protected variables
     public:
+        
         pawn() :piece{} {
             name = "pawn";
             point_value = 1;
         };
-
-        pawn(std::string init_position, std::string init_name, bool init_colour) : piece{init_position,"pawn", init_colour} {}
+        pawn(int init_position, std::string init_name, bool init_colour) : piece{init_position,"pawn", init_colour} {}
         virtual ~pawn() {};
-        
 };
 
 class board {
+    // copy and move operators
 
+    // The pieces array contains all the pieces used.
+    // The board contains the references to the point values and the position of the pieces.
+    // The board construction allows for the calculation of the score of the board easily
+    // the pieces pointers get the position modified and stores the actual position
+    // when a piece moves update the actual position and then update the integer array
+    private:
+        piece *pieces_array[32];
+        int *board;
+        int board[64]; // the index is col*(j-1) + (i-1)
+    public:
+        board() = default;
+        board() {} /// bookmark
+
+        int sum_board() {
+            int sum{0};
+            for (size_t i{0}; i<65; i++) {
+                sum += board[i];
+            }
+        }
+        
 };
